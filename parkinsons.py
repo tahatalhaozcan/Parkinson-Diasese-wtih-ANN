dd3d9c7c927c6c1148997b77ef1323be56b43145
tahatalhaozcan <48087953+tahatalhaozcan@users.noreply.github.com>
2023-03-20 12:21:28 +0300

    Update parkinsons.py

--- parkinsons.py
+++ parkinsons.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from keras.mıodel2s import Sequential
+from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.callbacks import EarlyStopping
 
@@ -48,7 +48,7 @@ x=df.drop(columns=['status'],axis=1) #verisetindeki bağımsız değişkenlerimi
 y=df['status'] #veri setindeki bağımlı değişken(hasta durumu)
 
 #eğitim ve test verilerini ayırmak
-from sklearn.mıodel2_selection import train_test_split
+from sklearn.model_selection import train_test_split
 x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=0)
 
 
@@ -60,28 +60,28 @@ X_test = sc.fit_transform(x_test)
 
 #yapay sinir ağı
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
-mıodel1 = Sequential()
-mıodel1.add(Dense(12, activation='relu', input_dim=22))
-mıodel1.add(Dense(12, activation='relu'))
-mıodel1.add(Dense(1, activation='sigmoid'))
-mıodel1.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-history1 = mıodel1.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test), callbacks=[es])
-y_pred = mıodel1.predict(X_test)
+model1 = Sequential()
+model1.add(Dense(12, activation='relu', input_dim=22))
+model1.add(Dense(12, activation='relu'))
+model1.add(Dense(1, activation='sigmoid'))
+model1.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+history1 = model1.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test), callbacks=[es])
+y_pred = model1.predict(X_test)
 y_pred = (y_pred > 0.5)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 
 #dropout ile overfitting durumu engelleme 
-mıodel2 = Sequential()
-mıodel2.add(Dense(12, activation='relu', input_dim=22))
-mıodel2.add(Dropout(0.2))
-mıodel2.add(Dense(12, activation='relu'))
-mıodel2.add(Dropout(0.2))
-mıodel2.add(Dense(1, activation='sigmoid'))
-mıodel2.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ['accuracy'])
-history2 = mıodel2.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test), callbacks=[es])
-y_pred = mıodel2.predict(X_test)
+model2 = Sequential()
+model2.add(Dense(12, activation='relu', input_dim=22))
+model2.add(Dropout(0.2))
+model2.add(Dense(12, activation='relu'))
+model2.add(Dropout(0.2))
+model2.add(Dense(1, activation='sigmoid'))
+model2.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ['accuracy'])
+history2 = model2.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test), callbacks=[es])
+y_pred = model2.predict(X_test)
 y_pred = (y_pred > 0.5) #hasta olan kişilerin döndürülmesi 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred) #karmaşıklık matrisi 
@@ -91,7 +91,7 @@ print(cm)
 #Grafikler
 plt.plot(history1.history['loss'])
 plt.plot(history1.history['val_loss'])
-plt.title('mıodel2 Loss')
+plt.title('model2 Loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper right')
@@ -99,7 +99,7 @@ plt.show()
 
 plt.plot(history2.history['accuracy'])
 plt.plot(history2.history['val_accuracy'])
-plt.title('mıodel2 Accuracy')
+plt.title('model2 Accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='lower right')
